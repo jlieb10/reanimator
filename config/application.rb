6b035feea9e612ae6c2414a4a5bc7b914d637ee7
GeorgeTaveras1231 <george.taveras1231@gmail.com>
2014-08-05 16:32:56 -0400

    installed rspec

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,10 @@ module Reanimator
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |gen|
+        # change the default test generator to rspec specs
+        gen.testing_framework :rspec
+    end
   end
 end
