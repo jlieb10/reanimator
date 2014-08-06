8a70e43d113a01d04090314400850309f177c753
GeorgeTaveras1231 <george.taveras1231@gmail.com>
2014-08-06 19:07:51 -0400

    Added line to save auth_id in users table Organized q_and_a.yml seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     iauth = auth.with_indifferent_access
     where(:email => iauth[:info][:email]).first_or_create do |u|
+      u.auth_id   = iauth[:uid]
       u.name      = iauth[:info][:name]
       u.image_url = iauth[:info][:image]
       u.provider  = iauth[:provider]
