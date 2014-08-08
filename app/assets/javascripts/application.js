c301e721d0f5cc751b1c388ba895ff6780d78e3d
Joshua T. Lieberman <joshuathomaslieberman@gmail.com>
2014-08-08 14:04:39 -0400

    Jquery Animator

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,25 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+//= require jquery.wordrotator
 //= require turbolinks
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+$(document).ready(function() {
+  var authors = ['Shakespeare', 'Tolstoy', 'Austen', 'Dickens', 'Woolf', 'Faulkner', 'Dickinson', 'Dostoyevsky', 'Tolkien', 'Joyce', 'Cervantes', 'Hemingway', 'Poe', 'Nabakov', 'Orwell', 'Christie', 'Márquez', 'Angelou', 'Morrison', 'Shelley', 'Dante', 'Beckett', 'Borges', 'Brontë', 'Chaucer', 'Conrad', 'Eliot', 'Kafka', 'Melville', 'Swift', 'Twain', 'Whitman'];
+
+  $(function () {
+    $('#authors').wordsrotator({
+      words: authors, // Array of words, it may contain HTML values
+      // randomize: true, //show random entries from the words array
+      // stopOnHover: false, //stop animation on hover
+      // changeOnClick: false, //force animation run on click
+      animationIn: "bounceIn", //css class for entrace animation
+      animationOut: "bounceOut", //css class for exit animation
+      speed: 1750 // delay in milliseconds between two words
+    });
+  });
+
+  $(function(){ $(document).foundation();
+  });
+});
