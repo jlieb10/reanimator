db170284e190728bf4a4d8939992faa10ac27d80
GeorgeTaveras1231 <george.taveras1231@gmail.com>
2014-08-08 11:00:52 -0400

    Facilitated the assignment of expectations to questions with the Question#expectation= method
    Facilitated the assignment of additional to question_options with the
    QuestionOption#additional_input= method
    
    Changed expectation in seed q_and_a.yml file

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,11 +2,21 @@ class Question < ActiveRecord::Base
   # these will translate to scopes named 
   # #expects_radio, #expects_checkbox etc.
   # and boolean helpers named #expects_radio?, #expects_checkbox? etc.
-  VALID_EXPECTATIONS = %w( radio checkbox text_area text_field  ).map { |f| "expects_#{f}" }
-  enum :expectation => VALID_EXPECTATIONS
+  BASE_EXPECTATIONS       = %w( none radio select checkbox )
+  VALID_EXPECTATIONS_ENUM = BASE_EXPECTATIONS.map { |f| "expects_#{f}" }
+  enum :expectation => VALID_EXPECTATIONS_ENUM
 
   validates :content, :task_id, :expectation, :presence => true
-  validates :expectation, :inclusion => VALID_EXPECTATIONS
+  validates :expectation, :inclusion => VALID_EXPECTATIONS_ENUM
 
   belongs_to :task
+
+  def expectation= val
+    # facilitatie the assignment of expectations
+    # ei.
+    # question.expectation = :none
+    # question.expectation = :radio
+    val = "expects_#{val}" if BASE_EXPECTATIONS.include? val.to_s
+    super(val)
+  end
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,4 +2,5 @@ class Submission < ActiveRecord::Base
   belongs_to :user
   belongs_to :question_option
   
+  # needs validation that is scoped to question_option, and references
 end
