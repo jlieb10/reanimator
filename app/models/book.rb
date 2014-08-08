61907869f6cb88825987b4c63cd31e3b7ae62f7c
GeorgeTaveras1231 <george.taveras1231@gmail.com>
2014-08-08 15:12:11 -0400

    slight shift in a comment line

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,6 @@ class Book < ActiveRecord::Base
   serialize :titles         , Array
   serialize :authors        , Array
   serialize :descriptions   , Array
-  #============================#
+  #==============================#
   serialize :links          , Hash
 end
