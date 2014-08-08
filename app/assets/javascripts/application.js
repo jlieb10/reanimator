33b654ee1ec0f4021618c074f1d569865ff7b2f5
Joshua T. Lieberman <joshuathomaslieberman@gmail.com>
2014-08-08 15:05:34 -0400

    Working on partials still

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,16 +18,12 @@
 //= require_tree .
 
 $(document).ready(function() {
-  var authors = ['Shakespeare', 'Tolstoy', 'Austen', 'Dickens', 'Woolf', 'Faulkner', 'Dickinson', 'Dostoyevsky', 'Tolkien', 'Joyce', 'Cervantes', 'Hemingway', 'Poe', 'Nabakov', 'Orwell', 'Christie', 'Márquez', 'Angelou', 'Morrison', 'Shelley', 'Dante', 'Beckett', 'Borges', 'Brontë', 'Chaucer', 'Conrad', 'Eliot', 'Kafka', 'Melville', 'Swift', 'Twain', 'Whitman'];
-
   $(function () {
-    $('#authors').wordsrotator({
-      words: authors, // Array of words, it may contain HTML values
-      // randomize: true, //show random entries from the words array
-      // stopOnHover: false, //stop animation on hover
-      // changeOnClick: false, //force animation run on click
-      animationIn: "bounceIn", //css class for entrace animation
-      animationOut: "bounceOut", //css class for exit animation
+    $('#authors1, #authors2').wordsrotator({
+      words: ['Shakespeare', 'Tolstoy', 'Austen', 'Dickens', 'Woolf', 'Faulkner', 'Dickinson', 'Dostoyevsky', 'Tolkien', 'Joyce', 'Cervantes', 'Hemingway', 'Poe', 'Nabakov', 'Orwell', 'Christie', 'Márquez', 'Angelou', 'Morrison', 'Shelley', 'Dante', 'Beckett', 'Borges', 'Brontë', 'Chaucer', 'Conrad', 'Eliot', 'Kafka', 'Melville', 'Swift', 'Twain', 'Whitman'], // Array of words, it may contain HTML values
+      randomize: true, //show random entries from the words array
+      animationIn: "fadeInDown", //css class for entrace animation
+      animationOut: "fadeOutDown", //css class for exit animation
       speed: 1750 // delay in milliseconds between two words
     });
   });
--- vendor/assets/javascripts/jquery.wordrotator.js
+++ vendor/assets/javascripts/jquery.wordrotator.js
@@ -9,7 +9,7 @@
  *
  * Version: 0.9.0
  */
- 
+
 (function ( $ ) {
 
 	$.fn.wordsrotator = function(options){
@@ -31,57 +31,57 @@
 			var el = $(this)
 			var cont = $("#" + el.attr("id"));
 			var array = [];
-			
+
 			//if array is not empty
 			if ((settings.words) || (settings.words instanceof Array)) {
-				array = $.extend(true, [], settings.words); 
+				array = $.extend(true, [], settings.words);
 
 				//In random order, need a copy of array
-				if (settings.randomize) array_bak = $.extend(true, [], array); 
-	
-		
+				if (settings.randomize) array_bak = $.extend(true, [], array);
+
+
 				listItem = 0
 				//if randomize pick a random value for the list item
-				if (settings.randomize) listItem = Math.floor(Math.random() * array.length)
+				if (settings.randomize) listItem = Math.floor(Math.random() * array.length - 1)
 
 				//init value into container
 				cont.html(array[listItem] );
-	
-			
+
+
 				// animation option
 				var rotate = function() {
-	
+
 					cont.html("<span class='wordsrotator_wordOut'><span>" + array[listItem] + "</span></span>");
-	
+
 					if (settings.randomize) {
 						//remove printed element from array
 						array.splice(listItem, 1);
 						//refill the array from his copy, if empty
 						if (array.length==0) array = $.extend(true, [], array_bak);
 						//generate new random number
-						listItem = Math.floor(Math.random() * array.length);
+						listItem = Math.floor(Math.random() * (array.length));
 					} else {
-						//if reached the last element of the array, reset the index 
+						//if reached the last element of the array, reset the index
 						if (array.length==listItem+1) listItem=-1;
 						//move to the next element
 						listItem++;
 					}
-	
+
 					$("<span class='wordsrotator_wordIn'>" + array[listItem] + "</span>").appendTo(cont);
 					cont.wrapInner("<span class='wordsrotator_words' />");
 					cont.find(".wordsrotator_wordOut").addClass("animated " + settings.animationOut);
 					cont.find(".wordsrotator_wordIn").addClass("animated " + settings.animationIn);
 				};
-	
-			
-		
+
+
+
 					cont.on("click", function() {
 						if (settings.changeOnClick) {
 							rotate();
 							return false;
 						};
 					});
-							
+
 					if (settings.autoLoop) {
 						var t = setInterval(rotate, settings.speed);
 						if (settings.stopOnHover) {
@@ -89,15 +89,15 @@
 								window.clearInterval(t)
 							},function() {
 								t = setInterval(rotate, settings.speed);
-			
-							});	  
+
+							});
 						};
 					}
-	
+
 				};
-				
+
 		  });
 	  }
-	 
+
 }( jQuery ));
 
