9724d4715871a30085b5f8c14e103481679a2509
Joshua T. Lieberman <joshuathomaslieberman@gmail.com>
2014-08-11 14:18:07 -0400

    Anticipating Merge Conflict in JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,7 @@
 //= require turbolinks
 //= require_tree .
 
-$(document).ready(function() {
+$(document).(function() {
   var authors = ['Shakespeare', 'Tolstoy', 'Austen', 'Dickens', 'Woolf', 'Faulkner', 'Dickinson', 'Dostoyevsky', 'Tolkien', 'Joyce', 'Cervantes', 'Hemingway', 'Poe', 'Nabakov', 'Orwell', 'Christie', 'Márquez', 'Angelou', 'Morrison', 'Shelley', 'Dante', 'Beckett', 'Borges', 'Brontë', 'Chaucer', 'Conrad', 'Eliot', 'Kafka', 'Melville', 'Swift', 'Twain', 'Whitman']
 
   function randomAuthor() {
@@ -25,14 +25,31 @@ $(document).ready(function() {
   };
 
   function updateAuthor() {
-    $('#authors1').text(randomAuthor()).fadeToggle(1000);
-    $('#authors2').text(randomAuthor()).fadeToggle(1000);
+    var auth = $('#authors1, #authors2');
+    var width = $('h1.')
+
+    if(auth.is(':visible')) {
+      auth.fadeOut(500, function(){
+        $(this).text(randomAuthor());
+        $(this).parent('h1').animate({width, 1000);
+      });
+    } else {
+      auth.fadeIn(500);
+    };
+
+    // if(auth2.is(':visible')) {
+    //   auth2.fadeOut(500, function(){
+    //     auth2.text(randomAuthor()+"?");
+    //   });
+    // } else {
+    //   auth2.fadeIn(500);
+    // };
   };
 
   updateAuthor();
 
-  setInterval(function(){updateAuthor()}, 1000);
+  setInterval(function(){updateAuthor()}, 500);
 
-  $(function(){ $(document).foundation();
+  $(document).foundation();
   });
 });
