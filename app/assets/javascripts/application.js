bd981a865dc3807a8ffcc2d4062bb680e9e7af7e
Joshua T. Lieberman <joshuathomaslieberman@gmail.com>
2014-08-11 11:04:30 -0400

    Some Front End

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,21 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
-//= require jquery.wordrotator
 //= require turbolinks
 //= require_tree .
 
 $(document).ready(function() {
-  // $(function () {
-  //   $('#authors1, #authors2').wordsrotator({
-  //     , // Array of words, it may contain HTML values
-  //     randomize: true, //show random entries from the words array
-  //     animationIn: "fadeInDown", //css class for entrace animation
-  //     animationOut: "fadeOutDown", //css class for exit animation
-  //     speed: 1750 // delay in milliseconds between two words
-  //   });
-  // });
-
   var authors = ['Shakespeare', 'Tolstoy', 'Austen', 'Dickens', 'Woolf', 'Faulkner', 'Dickinson', 'Dostoyevsky', 'Tolkien', 'Joyce', 'Cervantes', 'Hemingway', 'Poe', 'Nabakov', 'Orwell', 'Christie', 'Márquez', 'Angelou', 'Morrison', 'Shelley', 'Dante', 'Beckett', 'Borges', 'Brontë', 'Chaucer', 'Conrad', 'Eliot', 'Kafka', 'Melville', 'Swift', 'Twain', 'Whitman']
 
   function randomAuthor() {
@@ -36,7 +25,7 @@ $(document).ready(function() {
   };
 
   function updateAuthor() {
-    $('#authors1').css({position: 'absolute'}).animate({top: "+=50"},1000).text(randomAuthor());
+    $('#authors1').text(randomAuthor());
     $('#authors2').text(randomAuthor());
   };
 
