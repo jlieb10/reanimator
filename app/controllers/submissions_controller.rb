4b7708d7f516b52c78488ca54af2efaa6d17014f
GeorgeTaveras1231 <george.taveras1231@gmail.com>
2014-08-11 14:36:40 -0400

    slight changes in task partial and submissions controller

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -3,14 +3,15 @@ class SubmissionsController < ApplicationController
   def create
     @submission = current_user.submissions.new(submissions_params)
     if @submission.save
-      redirect_to root_path
+      redirect_to task_path(@submission.question.task)
     else
 
     end
   end
 
   private
-  def submissions_params
-    params.require(:submission).permit(:question_id, :option_id)
-  end
+
+    def submissions_params
+      params.require(:submission).permit(:question_id, :option_id)
+    end
 end
