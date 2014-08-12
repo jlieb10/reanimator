efc1cfdbdab22ebaa147997dd49210df97d65399
GeorgeTaveras1231 <george.taveras1231@gmail.com>
2014-08-12 11:08:35 -0400

    organized session btns

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,12 +3,6 @@ module ApplicationHelper
   # ei. #signed_in?, #current_user
   include SimpleAuthentication
 
-  def session_btn
-    if signed_in?
-      render "partials/signoutbtn"
-    else
-      render "partials/signinbtn"
-    end
-  end
+  
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,12 @@ module SessionsHelper
   def sign_in_path
     "/auth/google"
   end
+
+  def session_btn
+    if signed_in?
+      link_to "Sign Out", sign_out_path, :class => "small button round", :method => :delete
+    else
+      link_to "Sign in with Google", sign_in_path, :class => "small button round", :method => :post
+    end
+  end
 end
